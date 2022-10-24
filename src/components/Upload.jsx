3f69c3c4258290d92e43b8d602b878175dc04eed
Ramyashree Shetty <ramyashetty18042000@gmail.com>
2022-10-25 02:14:46 +0530

    final-update

--- src/components/Upload.jsx
+++ src/components/Upload.jsx
@@ -2,8 +2,7 @@ import React , {useState} from 'react';
 import S3 from 'react-aws-s3';
 import '../App.css';
 import { makeStyles } from '@mui/styles';
-import Button from '@mui/material/Button';
-import { Paper,LinearProgress} from '@mui/material';
+import { Paper,LinearProgress, FormControl, InputLabel, Select, MenuItem, Button} from '@mui/material';
 import AWS from 'aws-sdk';
 
 
@@ -28,6 +27,7 @@ const useStyles = makeStyles({
     color: "black",
     marginTop: "2%",
     align: 'center',
+    justifyContent: 'center',
     textAlign: 'center'
   },
   upBtn:{
@@ -36,7 +36,7 @@ const useStyles = makeStyles({
     marginTop: "1%",
     textAlign: 'center',
     backgroundColor: '#626ad8',
-    marginBottom:"3%"
+    marginBottom:"2%"
   },
   section:{
     textAlign: 'center',
@@ -51,29 +51,51 @@ const useStyles = makeStyles({
     fontSize:"11px"
   },
   divSection2: {
-    align: 'center',
     textAlign: 'center',
-    marginTop: "2%",
     backgroundColor:"#626ad82e",
-    margin:"3% 35% 3% 35%",
-    padding:"3%"
+    margin:"1% 35% 1% 35%",
+    padding:"3%",
+    display:"flex",
+    justifyContent:"center",
+    alignItems:"center",
+    flexDirection:"column"
   },
   appText2: {
     textDecoration: "none",
     color: "black",
     fontFamily: font,
-    marginTop: "3%"
+    marginTop: "2%"
   },
   upBtn2:{
     fontFamily: font,
     color: "#626ad8",
-    marginTop: "3%",
+    marginTop: "5%",
     textAlign: 'center',
+    justifyContent: "center",
     backgroundColor: 'white',
     '&:hover': {
         background: "#626ad8",
         color: "white",
-    },}
+    },},
+    option:{
+      display:"flex",
+      flexDirection:"row",
+      alignItems:"center",
+      justifyContent:"center",
+      marginTop:"3%",
+      fontFamily:font,
+    },
+    drop:{
+      width:"20%",
+      fontSize:"18px",
+      fontFamily:font,
+    },
+    circleBar:{
+      marginBottom:"10%",
+      alignContent:"center",
+      textAlign:"center",
+      marginTop:"2%",
+    }
 });
 
 // installed using npm install buffer --save
@@ -86,6 +108,14 @@ const Upload = () => {
     const [bar, setBar] = useState(0);
     const [prog, setProg] = useState('');
     const [fileval, setFileval] = useState("");
+    const [selected, setSelected] = useState('');
+    const delay = ms => new Promise(
+      resolve => setTimeout(resolve, ms)
+    );
+
+    const selectionChangeHandler = (event) => {
+      setSelected(event.target.value);
+    };
 
     const config = {
         bucketName: process.env.REACT_APP_BUCKET_NAME,
@@ -107,20 +137,27 @@ const Upload = () => {
     const uploadFile = async (file) => {
         const ReactS3Client = new S3(config);
 
+        if ( selected !== "" ) {
         ReactS3Client
-        .uploadFile(file, file.name)
+        .uploadFile(file,selected+"$"+file.name)
         .then(data => console.log(data.location))
         .catch(err => console.error(err))
 
         setFileval(file.name)
         setBar(bar + 100)
         setProg("Sucessful, you can download your translated file!")
+
+        }
+        else {
+          alert("Select language");
+        }
     }
 
-    const downloadFile = () => {
+    const downloadFile = async () => {
+        await delay(3000);
         var s3 = new AWS.S3();
         s3.getObject(
-        { Bucket: process.env.REACT_APP_BUCKET_NAME_OP, Key:fileval},
+        { Bucket: process.env.REACT_APP_BUCKET_NAME_OP, Key:selected+"$"+fileval},
         function (error, data) {
             if (error != null) {
             alert("Failed to retrieve an object: " + error);
@@ -130,17 +167,38 @@ const Upload = () => {
                 var blob = new Blob([data.Body], {type: "text/plain;charset=utf-8"});
                 FileSaver.saveAs(blob, "translated.txt");
                 window.location.reload()
-            }}
+                }
+            }
         );
-
     }
 
     return(
     <div className={classes.section}>
-        <Paper elevation={2} className={classes.divSection}>
+        <Paper elevation={2} className={classes.divSection} >
             <div className={classes.appText}>Upload your file to translate it into your desired language</div>
-            <input className={classes.inBtn} type="file" onChange={handleFileInput}/>
-            <br></br>
+            <div className={classes.option}>
+              <input className={classes.inBtn} type="file" onChange={handleFileInput}/>
+              <FormControl fullWidth className={classes.drop} variant="standard" size="small">
+                <InputLabel id="demo-simple-select-label"  sx={{fontFamily:font, fontSize:"14px"}}>Select Language</InputLabel>
+                <Select
+                  labelId="demo-simple-select-label"
+                  id="demo-simple-select"
+                  value={selected}
+                  label="language"
+                  onChange={selectionChangeHandler}>
+                  <MenuItem value={'en'} sx={{fontFamily:font, fontSize:"14px"}}>English</MenuItem>
+                  <MenuItem value={'hi'} sx={{fontFamily:font, fontSize:"14px"}}>Hindi</MenuItem>
+                  <MenuItem value={'de'} sx={{fontFamily:font, fontSize:"14px"}}>German</MenuItem>
+                  <MenuItem value={'it'} sx={{fontFamily:font, fontSize:"14px"}}>Italian</MenuItem>
+                  <MenuItem value={'ja'} sx={{fontFamily:font, fontSize:"14px"}}>Japanese</MenuItem>
+                  <MenuItem value={'ko'} sx={{fontFamily:font, fontSize:"14px"}}>Korean</MenuItem>
+                  <MenuItem value={'es'} sx={{fontFamily:font, fontSize:"14px"}}>Spanish</MenuItem>
+                  <MenuItem value={'fr'} sx={{fontFamily:font, fontSize:"14px"}}>French</MenuItem>
+                  <MenuItem value={'ar'} sx={{fontFamily:font, fontSize:"14px"}}>Arabic</MenuItem>
+                  <MenuItem value={'zh'} sx={{fontFamily:font, fontSize:"14px"}}>Chinese</MenuItem>
+                </Select>
+              </FormControl>
+            </div>
         </Paper>
 
         <LinearProgress  variant="determinate" className={classes.bar} value={bar}/>
